b0b1ab6cb6552233540ebe84150c5c3b38851266
AlisherMukhambetzhanov <53463211+AlisherMukhambetzhanov@users.noreply.github.com>
2022-03-31 16:22:30 +0600

    Update pizzas.js

--- src/redux/reducers/pizzas.js
+++ src/redux/reducers/pizzas.js
@@ -1,5 +1,16 @@
 const initialState = {
-    items: [],
+    items: [
+    {
+      "id": 0,
+      "imageUrl": "https://dodopizza.azureedge.net/static/Img/Products/f035c7f46c0844069722f2bb3ee9f113_584x584.jpeg",
+      "name": "Пепперони Фреш с перцем",
+      "types": [0, 1],
+      "sizes": [26, 30, 40],
+      "price": 803,
+      "category": 0,
+      "rating": 4
+    },
+    ],
     isLoaded: false
 }
 
